f303ca11b02aaec3e4c31e3f30d49640f7d90e17
fahmiam2 <fahmiam271@gmail.com>
2024-01-01 21:39:58 +0700

    feat: add sample annotated image

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -28,15 +28,15 @@ export default function Guide() {
   };
 
   return (
-    <section className="container mx-auto mb-3 bg-slate-400 rounded-xl">
-      <h2 className="text-3xl font-bold my-10 text-center">
+    <section className="container mx-auto mb-10 bg-pictonBlue rounded-xl">
+      <h2 className="text-3xl font-bold my-10 text-center text-white">
         How to use GoDetect Effortlessly?
       </h2>
       <div className="flex flex-col xl:flex-row px-5 xl:px-20 mb-20 mx-5 xl:mx-20 gap-10">
         <div className="w-full xl:w-1/2 flex items-center justify-center relative mb-5 xl:mb-0">
           {/* Before Image */}
           <motion.img
-            src="../../public/sample-image.jpg"
+            src="/public/sample-image.jpg"
             className="min-h-unit-7xl max-w-full"
             alt="Before"
             initial={{ width: isAfter ? "0%" : "50%" }}
@@ -46,7 +46,7 @@ export default function Guide() {
 
           {/* After Image */}
           <motion.img
-            src="../../public/sample-annotated-image.jpg"
+            src="/public/sample-annotated-image.jpg"
             className="min-h-unit-7xl max-w-full"
             alt="After"
             initial={{ width: isAfter ? "50%" : "0%" }}
@@ -57,9 +57,9 @@ export default function Guide() {
           {/* Button to Toggle Image */}
           <button
             onClick={toggleImage}
-            className="bg-blue-500 text-white px-4 py-2 rounded absolute bottom-0 left-0"
+            className="bg-indigo-800 text-white px-4 py-2 rounded-lg absolute bottom-0 left-0"
           >
-            Toggle Image
+            Show Sample
           </button>
         </div>
         <div className="w-full xl:w-1/2 flex flex-col gap-10">
