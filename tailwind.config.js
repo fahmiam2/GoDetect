e5ab72f9e6a4ba896c134fc47e1a44d174cd487b
fahmiam2 <fahmiam271@gmail.com>
2024-01-01 21:27:50 +0700

    chore: add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,26 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        darkPurple: "#260C1A",
+        liver: "#5F4842",
+        blueMunsell: "#068D9D",
+        pictonBlue: "#00A7E1",
+      },
+      color: {
+        darkPurple: "#260C1A",
+        liver: "#5F4842",
+        blueMunsell: "#068D9D",
+        pictonBlue: "#00A7E1",
+      },
+      text: {
+        darkPurple: "#260C1A",
+        liver: "#5F4842",
+        blueMunsell: "#068D9D",
+        pictonBlue: "#00A7E1",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
