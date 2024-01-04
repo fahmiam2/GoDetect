4f09fb9bf833abafffadf9ef0d5aefd56a54a735
fahmiam2 <fahmiam271@gmail.com>
2024-01-04 16:58:00 +0700

    feat(footer): make a responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,28 @@ import { IconGithub, IconInstagram, IconLinkedin } from "./SocialMediaIcon";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-5">
-      <div className="container mx-auto flex justify-between items-center">
+    <footer className="bg-black py-5 text-white">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-5 md:flex-row md:gap-0">
         <div className="text-left">
           <p className="text-sm">&copy; 2024 GoDetect. All Rights Reserved.</p>
         </div>
         <div className="flex space-x-4 px-5">
           <a
-            href="https://www.instagram.com/"
+            href="https://www.instagram.com/fahmi_am2"
             target="_blank"
             rel="noopener noreferrer"
           >
             <IconInstagram />
           </a>
           <a
-            href="https://www.linkedin.com/"
+            href="https://www.linkedin.com/in/fahmiam2/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <IconLinkedin />
           </a>
           <a
-            href="https://github.com/"
+            href="https://github.com/fahmiam2"
             target="_blank"
             rel="noopener noreferrer"
           >
