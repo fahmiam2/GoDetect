41b28757aaf9f07a7a2f43f0e1b2f78085d6c54c
fahmiam2 <fahmiam271@gmail.com>
2024-01-05 18:02:16 +0700

    feat(404): add 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import Faqs from "./pages/Faqs";
 import ApiDoc from "./pages/Apidoc";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="apis" element={<ApiDoc />} />
           <Route path="faqs" element={<Faqs />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </NextUIProvider>
     </BrowserRouter>
